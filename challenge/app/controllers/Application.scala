63992fbb3f41d680e449043a1b674eed90d799a4
Robert Austin <robert@oatlab.com>
2012-10-26 01:52:46 -0400

    using a custom PartHandler

--- challenge/app/controllers/Application.scala
+++ challenge/app/controllers/Application.scala
@@ -8,6 +8,8 @@ import models.Purchaser
 import play.api.libs.iteratee.{Iteratee, Input}
 import play.api.libs.iteratee.Parsing
 import play.api.libs.iteratee.Enumeratee
+import play.api.libs.Files.TemporaryFile
+import java.io.FileOutputStream
 
 object Application extends Controller {
   
@@ -27,17 +29,32 @@ object Application extends Controller {
 	def deletePurchaser(id: Long) = TODO
 	def newPurchaser = TODO
 	
-	def upload = Action(parse.multipartFormData) { request =>
+	def upload = Action(parse.multipartFormData(handleFilePartAsTemporaryFile) ) { request =>
 		request.body.file("picture").map { picture =>
 			import java.io.File
 			val filename = picture.filename 
 			val contentType = picture.contentType
-			picture.ref.moveTo(new File("/tmp/picture"))
+			picture.ref.moveTo(new File("/tmp/picture2"))
 			Ok("File uploaded")
 		}.getOrElse {
 			Redirect(routes.Application.index).flashing(
 				"error" -> "Missing file"
 			)
+		}
+	}
+	
+
+	def handleFilePartAsTemporaryFile: parse.Multipart.PartHandler[MultipartFormData.FilePart[TemporaryFile]] = {
+		parse.Multipart.handleFilePart {
+			case parse.Multipart.FileInfo(partName, filename, contentType) =>
+				val tempFile = TemporaryFile("multipartBody", "asTemporaryFile")
+				Iteratee.fold[Array[Byte], FileOutputStream](new java.io.FileOutputStream(tempFile.file)) { (os, data) =>
+					os.write(data)
+						os
+				}.mapDone { os =>
+					os.close()
+						tempFile
+				}
 		}
 	}
 	
