da6e75bac5bc2c453ef3cc65fa4cd92a6eccd654
Robert Austin <robert@oatlab.com>
2012-10-26 02:28:33 -0400

    parsing the tab delimited file as its uploaded

--- challenge/app/controllers/Application.scala
+++ challenge/app/controllers/Application.scala
@@ -8,8 +8,14 @@ import models.Purchaser
 import play.api.libs.iteratee.{Iteratee, Input}
 import play.api.libs.iteratee.Parsing
 import play.api.libs.iteratee.Enumeratee
-import play.api.libs.Files.TemporaryFile
-import java.io.FileOutputStream
+import play.api.libs.iteratee.{Iteratee, Input}
+import play.api.libs.concurrent.Promise
+import play.api.libs.iteratee.Input.{El, EOF, Empty}
+import au.com.bytecode.opencsv.CSVReader
+import java.io.StringReader
+import scala.collection.JavaConversions._
+import play.api.libs.json.Json._
+import play.api.libs.ws.WS
 
 object Application extends Controller {
   
@@ -29,6 +35,7 @@ object Application extends Controller {
 	def deletePurchaser(id: Long) = TODO
 	def newPurchaser = TODO
 	
+	/*
 	def upload = Action(parse.multipartFormData(handleFilePartAsTemporaryFile) ) { request =>
 		request.body.file("picture").map { picture =>
 			import java.io.File
@@ -43,19 +50,64 @@ object Application extends Controller {
 		}
 	}
 	
-
 	def handleFilePartAsTemporaryFile: parse.Multipart.PartHandler[MultipartFormData.FilePart[TemporaryFile]] = {
 		parse.Multipart.handleFilePart {
 			case parse.Multipart.FileInfo(partName, filename, contentType) =>
 				val tempFile = TemporaryFile("multipartBody", "asTemporaryFile")
 				Iteratee.fold[Array[Byte], FileOutputStream](new java.io.FileOutputStream(tempFile.file)) { (os, data) =>
 					os.write(data)
-						os
+					os
 				}.mapDone { os =>
 					os.close()
-						tempFile
+					tempFile
 				}
 		}
 	}
+	*/
 	
+  def upload = Action(BodyParser(rh => new CsvIteratee(isFirst = true))) {
+    request =>
+      Ok("File Processed")
+  }
+
+	
+}
+case class CsvIteratee(state: Symbol = 'Cont, input: Input[Array[Byte]] = Empty, lastChunk: String = "", isFirst: Boolean = false) extends Iteratee[Array[Byte], Either[Result, String]] {
+  def fold[B](
+               done: (Either[Result, String], Input[Array[Byte]]) => Promise[B],
+               cont: (Input[Array[Byte]] => Iteratee[Array[Byte], Either[Result, String]]) => Promise[B],
+               error: (String, Input[Array[Byte]]) => Promise[B]
+               ): Promise[B] = state match {
+    case 'Done =>
+      done(Right(lastChunk), Input.Empty)
+
+    case 'Cont => cont(in => in match {
+      case in: El[Array[Byte]] => {
+        // Retrieve the part that has not been processed in the previous chunk and copy it in front of the current chunk
+        val content = lastChunk + new String(in.e)
+        val csvBody =
+          if (isFirst)
+            // Skip http header if it is the first chunk
+            content.drop(content.indexOf("\r\n\r\n") + 4)
+          else content
+        val csv = new CSVReader(new StringReader(csvBody), '\t')
+        val lines = csv.readAll
+        // Process all lines excepted the last one since it is cut by the chunk
+        for (line <- lines.init)
+          processLine(line)
+        // Put forward the part that has not been processed
+        val last = lines.last.toList.mkString(";")
+        copy(input = in, lastChunk = last, isFirst = false)
+      }
+      case Empty => copy(input = in, isFirst = false)
+      case EOF => copy(state = 'Done, input = in, isFirst = false)
+      case _ => copy(state = 'Error, input = in, isFirst = false)
+    })
+
+    case _ =>
+      error("Unexpected state", input)
+
+  }
+
+  def processLine(line: Array[String]) = println(line(1))
 }
