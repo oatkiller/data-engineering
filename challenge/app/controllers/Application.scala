b5c40d70ae0390f261a85dc7e9e73414cd6e0f3c
Robert Austin <robert@oatlab.com>
2012-10-25 23:05:55 -0400

    adding some basic crud functionality as a sanity check

--- challenge/app/controllers/Application.scala
+++ challenge/app/controllers/Application.scala
@@ -3,10 +3,41 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import play.api.data._
+import play.api.data.Forms._
+
+import models.Purchaser
+
 object Application extends Controller {
   
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-  
+	def index = Action {
+		Redirect(routes.Application.purchase_orders)
+	}
+	
+  def purchase_orders = Action {
+	
+		val purchaserForm = Form(
+			"name" -> nonEmptyText
+		)
+		
+		Ok(views.html.index(Purchaser.all(), purchaserForm))
+	}
+	
+	def deletePurchaser(id: Long) = TODO
+	def newPurchaser = TODO
+	
+	def upload = Action(parse.multipartFormData) { request =>
+		request.body.file("picture").map { picture =>
+			import java.io.File
+			val filename = picture.filename 
+			val contentType = picture.contentType
+			picture.ref.moveTo(new File("/tmp/picture"))
+			Ok("File uploaded")
+		}.getOrElse {
+			Redirect(routes.Application.index).flashing(
+				"error" -> "Missing file"
+			)
+		}
+	}
+	
 }
---
+++ challenge/app/models/Purchaser.scala
@@ -0,0 +1,21 @@
+package models
+import anorm._
+import anorm.SqlParser._
+import play.api.db._
+import play.api.Play.current
+
+case class Purchaser(id: Long, name: String)
+
+object Purchaser {
+	
+	val purchaser = {
+		get[Long]("id") ~
+		get[String]("name") map {
+			case id~name => Purchaser(id, name)
+		}
+	}
+	
+	def all(): List[Purchaser] = DB.withConnection { implicit c =>
+		SQL("select * from purchaser").as(purchaser *)
+	}
+}
